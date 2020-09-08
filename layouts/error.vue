da03e94781a22d045dce2531cc373230fa364664
Aisen60 <caixusheng60@gmail.com>
2020-09-08 22:13:43 +0800

    feat: 完善了默认布局，添加了页尾和返回顶部按钮

---
+++ layouts/error.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1 v-if="error.statusCode === 404">页面不存在</h1>
+    <h1 v-else>页面发生了一个错误</h1>
+    <nuxt-link to="/">返回首页</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['error'],
+}
+</script>
