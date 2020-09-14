3dd2a75708965683fb70fbe8d46470b7c5c87824
Aisen60 <caixusheng60@gmail.com>
2020-09-14 16:28:51 +0800

    feat: 添加 about 页面

---
+++ pages/about.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="page-about">
+    <div class="page-about-section">
+      <p class="page-about-text-right">JUST CALL ME :C</p>
+      <p>THE WECHAT 👉 {{ config.WECHAT }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  head() {
+    return {
+      title: `${this.config.SIMPLE_NAME} | About`,
+    }
+  },
+}
+</script>
