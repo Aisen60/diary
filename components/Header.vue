4200ef17802c40918f97836bbaaea6e5dac4699c
Aisen60 <caixusheng60@gmail.com>
2020-09-14 16:28:20 +0800

    feat: 重命名原 Banner 组件 为 Header，添加 nav 组件

---
+++ components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="app-header" :class="{ 'app-header-fixed': fixed }">
+    <div class="container app-header-section app-header-nav">
+      <h1>
+        <nuxt-link to="/"> {{ config.SIMPLE_NAME.toUpperCase() }}</nuxt-link>
+      </h1>
+      <Nav />
+    </div>
+    <div class="app-header-bio-box">
+      <div class="container bio-wrapper">
+        <h3>BLOG @ {{ config.FULL_NAME }}</h3>
+        <p>{{ config.BIO }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Header',
+  props: {
+    fixed: {
+      type: Boolean,
+      default: false,
+    },
+  },
+}
+</script>
---
+++ components/Nav.vue
@@ -0,0 +1,22 @@
+<template>
+  <ul class="nav-link">
+    <li v-for="item in nav" :key="item.name + '-' + item.path">
+      <nuxt-link v-if="!item.external" :to="{ path: item.path }">
+        {{ item.name }}
+      </nuxt-link>
+      <a v-else :href="github" target="_blank">Github</a>
+    </li>
+  </ul>
+</template>
+
+<script>
+import { APP_NAV, GITHUB_AISEN60 } from '../config'
+export default {
+  data() {
+    return {
+      nav: APP_NAV,
+      github: GITHUB_AISEN60,
+    }
+  },
+}
+</script>
