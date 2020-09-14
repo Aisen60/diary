f0c0409896a07bab6a9a4df042589e13b35e5eb2
Aisen60 <caixusheng60@gmail.com>
2020-09-14 22:02:07 +0800

    chore: add log

--- plugins/main.js
+++ plugins/main.js
@@ -11,8 +11,18 @@ Object.keys(filters).forEach((key) => {
 
 Vue.use(methods)
 
+// eslint-disable-next-line no-console
 console.log(
   `%c ${APP_TITLE} %c current version: ${APP_VERSION}`,
   'background: #35495e; padding: 1px; color: #fff',
   'background: #2b61c5; padding: 1px; color: #fff'
 )
+
+// eslint-disable-next-line no-console
+console.log('hello, why are you open the devtools?')
+
+// eslint-disable-next-line no-console
+console.log('If it helps you or if you like it, can you give me a star? thanks')
+
+// eslint-disable-next-line no-console
+console.log('repository url: https://github.com/Aisen60/blog')
