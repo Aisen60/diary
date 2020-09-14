0afa0328e73bc308a4db35ec9f2e9611f1536390
Aisen60 <caixusheng60@gmail.com>
2020-09-14 16:30:17 +0800

    chore: 修改原有的 post 页面，迁移至posts文件夹下

---
+++ pages/posts/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="page-postsList">
+    <Header />
+    <div class="page-postsList-wrapper">
+      <div class="container">
+        <div class="row card-box">
+          <div
+            v-for="post in postsData"
+            :key="post.id"
+            class="col-lg-4 col-md-6 col-sm-6"
+          >
+            <Card :card="post" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Api from '../../services/index'
+export default {
+  layout: 'posts',
+  name: 'PostsList',
+  async asyncData({ app, store }) {
+    const { data: postsData } = await Api.getIssues()
+    return {
+      postsData,
+    }
+  },
+  head() {
+    return {
+      title: `${this.config.SIMPLE_NAME} | Posts`,
+    }
+  },
+}
+</script>
